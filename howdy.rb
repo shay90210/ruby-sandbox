b7e660eba164eba3bc83bf9d8d6992095bcef11c
Shannon Tatianna Bentley <bentley.shannon@yahoo.com>
2024-02-05 00:52:27 +0000

    Created a gemfile

--- howdy.rb
+++ howdy.rb
@@ -5,3 +5,18 @@ pp my_string
 
 require "./goodbye.rb"
 require "active_support/all"
+
+pp 1.ordinalize
+pp 2.ordinalize
+pp 53.ordinalize
+pp 2009.ordinalize
+pp -21.ordinalize
+pp -134.ordinalize
+
+pp "table".pluralize
+pp "ruby".pluralize
+pp "equipment".pluralize
+
+pp "tables".singularize
+pp "rubies".singularize
+pp "equipments".singularize
