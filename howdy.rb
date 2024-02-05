1fa6c24018ef17e1b7710458829d43e7f0446b2a
Shannon Tatianna Bentley <bentley.shannon@yahoo.com>
2024-02-05 01:03:53 +0000

    Created gets code

--- howdy.rb
+++ howdy.rb
@@ -6,6 +6,7 @@ pp my_string
 require "./goodbye.rb"
 require "active_support/all"
 
+'''
 pp 1.ordinalize
 pp 2.ordinalize
 pp 53.ordinalize
@@ -20,3 +21,8 @@ pp "equipment".pluralize
 pp "tables".singularize
 pp "rubies".singularize
 pp "equipments".singularize
+'''
+pp "What's your name?"
+their_name = gets
+
+pp "Hello, " + their_name + "!"
