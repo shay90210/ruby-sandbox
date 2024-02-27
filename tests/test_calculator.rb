d25ad0472209f9bfcb19fce44bee29a43aaffb58
Shannon Tatianna Bentley <bentley.shannon@yahoo.com>
2024-02-27 03:23:24 +0000

    created subtration test

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -8,6 +8,9 @@ class TestCalculator < Minitest:: Test
     calculator = Calculator.new
     assert_equal 4, calculator.add(2, 2), "Addition method failed"
   end
+
+  def test_subtration
+    calculator = Calculator.new
+    assert_equal 0, calculator.subtract(2, 2), "Subtraction method failed"
+  end
 end
-
-
